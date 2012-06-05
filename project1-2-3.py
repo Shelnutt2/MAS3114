161c65d0d066e2f934b81c605e16e6d09dad0891
John Caruzzo <caruzzo@gmail.com>
2012-06-05 18:42:27 -0400

    Further ref support

--- project1-2-3.py
+++ project1-2-3.py
@@ -13,16 +13,40 @@ import argparse
 parser = argparse.ArgumentParser(description='This is a program to draw a 2d plane based on a set of vectors.')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.5')
 parser.add_argument('Rows', metavar='Rows', type=eval, nargs='+', help='Rows of the matrix')
-parser.add_argument('-s', '--solve', action='store', type=bool, metavar='Rows', type=eval, nargs='+', help='Rows of the matrix')
+parser.add_argument('-s', '--solve', action='store', type=bool, metavar='solve', help='Solve for variables')
+parser.add_argument('-r', '--reduce', action='store', type=bool, metavar='reduce', help='reduce to reduced row echlon form')
 
 #initialize the arguments
 args = parser.parse_args()
 
 #Check to make sure only two vectors are inputed
-if not len(args.Vectors) == 2:
+if not len(args.Rows) == 2:
     parser.error("Please input atleast two rows")
 
-
+def ref( M ):
+   for rnum in range(len(M)) :
+      if M[rnum][rnum] == 0 :
+	     #rnump = 0
+         if rnum +1 <= len(M) :
+		    rnump = rnum+1
+         else:
+		    rnump = rnum
+         mtemp = M[rnum]
+         M[rnum] = M[rnump]
+         M[rnump] = mtemp
+      if not M[rnum][rnum] == 0:
+         print M[rnum]
+         print M[rnum][rnum]
+         number = M[rnum][rnum]
+         print number
+         M[rnum] = [ mrow/number for mrow in M[rnum] ]
+      for nrnum in range(len(M)-1):
+         if not M[nrnum+1][rnum] == 0:
+            var2 = M[nrnum+1][rnum]
+            #var3 = M[rnum]
+            for digit in range(len(M[rnum+1])-1):
+               M[nrnum+1][digit] = [M[nrnum+1][digit] - var2*M[rnum][digit]]
+	
 def rref( M ):
     if not M: return
     lead = 0
@@ -48,15 +72,11 @@ def rref( M ):
                 M[i] = [ iv - lv*rv for rv,iv in zip(M[r],M[i])]
         lead += 1
 
-def ref( M ) :
-   
 
-
-mat = []
-for r in args.Rows:
-   mat.append(r)
  
-rref( mtx )
- 
-
-
+if(args.solve):
+  p=1#solve functions
+elif args.reduce:
+   rref(args.Rows)
+else:
+   ref(args.Rows)
